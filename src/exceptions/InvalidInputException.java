349e7f9a5f5c1a64d9185dcb38d63ca4b43de2ae
nurbol amangeldinov <nurbolamangeldinov10@gmail.com>
2025-10-26 14:20:46 +0500

    Add InvalidInputException for input validation errors

---
+++ src/exceptions/InvalidInputException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
